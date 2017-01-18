72e807c667687ceeaf6c2c2cbe0572f9406849b9
Michael Fields <mfields@clcohio.org>
2017-01-18 10:23:33 -0500

    switched from images to font awesome arrows

--- src/Carousels/App_Start/BundleConfig.cs
+++ src/Carousels/App_Start/BundleConfig.cs
@@ -18,12 +18,17 @@ namespace Carousels
                         "~/Scripts/jquery.unobtrusive*",
                         "~/Scripts/jquery.validate*"));
 
+            bundles.Add(new ScriptBundle("~/bundles/js").Include(
+                        "~/Scripts/clc-carousel.js",
+                        "~/Scripts/jcarousel.min.js"));
+
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                        "~/Content/font-awesome/css/font-awesome.min.css"));
 
             bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
                         "~/Content/themes/base/jquery.ui.core.css",
