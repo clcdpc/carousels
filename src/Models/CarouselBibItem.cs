e409d4cd76d78813c6fa0e23e6f3826f74b62462
mtfields <mfields@clcohio.org>
2026-05-14 11:54:54 -0400

    Pass requested cover size through controller to image provider

--- src/Models/CarouselBibItem.cs
+++ src/Models/CarouselBibItem.cs
@@ -8,9 +8,9 @@
         public string Isbn { get; set; }
         public string Upc { get; set; }
 
-        public CarouselItem ToCarouselItem(ICoverImageProvider coverImageProvider, ICatalogLinkProvider catalogLinkProvider)
+        public CarouselItem ToCarouselItem(ICoverImageProvider coverImageProvider, ICatalogLinkProvider catalogLinkProvider, CoverImageSize size)
         {
-            return new CarouselItem { Title = new string(Title.Replace(@"\'", @"'").Replace(@"\""", @"""").Take(50).ToArray()).Replace("'", @"\'").Replace("\"", @"\""") + (Title.Length > 50 ? "..." : ""), CatalogLink = catalogLinkProvider.GetLink(this), ImageUrl = coverImageProvider.GetImageUrl(this, CoverImageSize.S) };
+            return new CarouselItem { Title = new string(Title.Replace(@"\'", @"'").Replace(@"\""", @"""").Take(50).ToArray()).Replace("'", @"\'").Replace("\"", @"\""") + (Title.Length > 50 ? "..." : ""), CatalogLink = catalogLinkProvider.GetLink(this), ImageUrl = coverImageProvider.GetImageUrl(this, size) };
         }
     }
 }
